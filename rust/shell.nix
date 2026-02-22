9f8e07055ff75342ffa61d33eb53b574c9f4a958
zarov <joao_rosa@tutamail.com>
2026-02-22 17:40:02 -0300

    test

--- rust/shell.nix
+++ rust/shell.nix
@@ -1,13 +1,12 @@
 { pkgs ? import <nixpkgs> { overlays = [ (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz")) ]; } }:
 
 pkgs.mkShell {
-  
+
   packages = [
     (pkgs.rust-bin.stable."1.82.0".default.override {
       extensions = [ "rust-src" ];
     })
 
-    pkgs.git
     pkgs.pkg-config
     pkgs.openssl
   ];
